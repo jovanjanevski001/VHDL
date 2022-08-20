812e9e2fa0f90dde82fa6e355d86a4d22279f3c6
Jovan Janevski <32399896+jovanjanevski001@users.noreply.github.com>
2022-08-19 23:09:59 -0400

    Update top_level.vhd
    fixed an inference of latches for led0_dp, led1_dp, led2_dp, led3_dp, led4_dp, led5_dp outputs.
    used a 'for generate' to simplify redundant code,. The intended circuit was still synthesized.

--- decoder7seg/top_level.vhd
+++ decoder7seg/top_level.vhd
@@ -12,20 +12,20 @@ entity top_level is
 		ledr	:	out std_logic_vector(9 downto 0);		-- LEDs above the switches
 		
 		-- 7seg displays 
-		led0	:	out	std_logic_vector(6 downto 0);
-		led1	:	out	std_logic_vector(6 downto 0);
-		led2	:	out	std_logic_vector(6 downto 0);
-		led3	:	out	std_logic_vector(6 downto 0);
-		led4	:	out	std_logic_vector(6 downto 0);
-		led5	:	out	std_logic_vector(6 downto 0);
+		led0	:	out std_logic_vector(6 downto 0);
+		led1	:	out std_logic_vector(6 downto 0);
+		led2	:	out std_logic_vector(6 downto 0);
+		led3	:	out std_logic_vector(6 downto 0);
+		led4	:	out std_logic_vector(6 downto 0);
+		led5	:	out std_logic_vector(6 downto 0);
 		
 		-- 7seg dp displays
-		led0_dp	:	out std_logic;
-		led1_dp	:	out std_logic;
-		led2_dp	:	out std_logic;
-		led3_dp	:	out std_logic;
-		led4_dp	:	out std_logic;
-		led5_dp	:	out std_logic
+		led0_dp	:	out	std_logic;
+		led1_dp	:	out	std_logic;
+		led2_dp	:	out	std_logic;
+		led3_dp	:	out	std_logic;
+		led4_dp	:	out	std_logic;
+		led5_dp	:	out	std_logic
 		
 	);
 end top_level;
@@ -83,59 +83,38 @@ begin -- STR
 			input 	=> switch(3 downto 0),
 			output	=> led5
 		);
+
+
+	process(button)
+	begin	
+		if(button(0) = '0') then
+			led0_dp <= '0';
+			led1_dp <= '0';  
+			led2_dp <= '0';
+		else
+			led0_dp <= '1';
+			led1_dp <= '1';  
+			led2_dp <= '1';
+		end if;
+		
+		if(button(1) = '0') then
+			led3_dp <= '0';
+			led4_dp <= '0';
+			led5_dp <= '0';
+		else
+			led3_dp <= '1';
+			led4_dp <= '1';
+			led5_dp <= '1';
+		end if;
 	
-	
-	led0_dp <= '0' when button(0) = '0' else
-			   '1' when button(0) = '1';
-	
-	led1_dp <= '0' when button(0) = '0' else
-			   '1' when button(0) = '1';
-			   
-	led2_dp <= '0' when button(0) = '0' else
-			   '1' when button(0) = '1';
-	
-	
-	led3_dp <= '0' when button(1) = '0' else
-			   '1' when button(1) = '1';
-	
-	led4_dp <= '0' when button(1) = '0' else
-			   '1' when button(1) = '1';
-			   
-	led5_dp <= '0' when button(1) = '0' else
-			   '1' when button(1) = '1';
-	
+	end process;
 	
 	-- Connecting the LEDs above the switches, 
 	--	if switch is flipped on, then the LED above it will turn on
-	ledr(0) <= '1' when switch(0) = '1' else
-			   '0' when switch(0) = '0';
+	FOR_GENERATE: for i in 0 to 9 generate
+		ledr(i) <= '1' when switch(i) = '1' else
+				   '0' when switch(i) = '0';
+	end generate FOR_GENERATE;
 	
-	ledr(1) <= '1' when switch(1) = '1' else
-			   '0' when switch(1) = '0';
-			  
-	ledr(2) <= '1' when switch(2) = '1' else
-			   '0' when switch(2) = '0';
-			  
-	ledr(3) <= '1' when switch(3) = '1' else
-			   '0' when switch(3) = '0';	
-			  
-	ledr(4) <= '1' when switch(4) = '1' else
-			   '0' when switch(4) = '0';
 	
-	ledr(5) <= '1' when switch(5) = '1' else
-			   '0' when switch(5) = '0';
-			  
-	ledr(6) <= '1' when switch(6) = '1' else
-			   '0' when switch(6) = '0';
-			  
-	ledr(7) <= '1' when switch(7) = '1' else
-			   '0' when switch(7) = '0';		
-			  
-	ledr(8) <= '1' when switch(8) = '1' else
-			   '0' when switch(8) = '0';
-			  
-	ledr(9) <= '1' when switch(9) = '1' else
-			   '0' when switch(9) = '0';	
-
-			   
 end STR;
